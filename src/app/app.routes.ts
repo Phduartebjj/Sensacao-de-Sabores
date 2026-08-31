ee56997bb4e24415a5812d60f2901a2418f597ea
maoel <pedrohduarte007@gmail.com>
2026-08-30 22:57:03 -0300

    feat: add home and product details components with routing and styles

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: HomeComponent,
+    },
+    {
+        path: 'produto/:id',
+        component: ProductDetailsComponent,
+    },
+    {
+        path: '**',
+        redirectTo: '',
+    }
+];
