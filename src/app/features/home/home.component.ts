c6c2b52c0e0d5005d3c7eec20ce5559c8b0e6109
maoel <pedrohduarte007@gmail.com>
2026-08-31 21:07:36 -0300

    feat(home): add products section and integrate product card

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,11 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { HeroComponent } from './hero/hero';
 import { BenefitsComponent } from './benefits/benefits';
+import { ProductCardComponent } from '../../shared/components/product-card.component/product-card.component';
+import { ProductService } from '../../core/services/product/product.service';
+import { CartService } from '../../core/services/cart/cart.service';
+import { Product } from '../../core/models/product.model';
 
 @Component({
-  imports: [HeroComponent, BenefitsComponent],
   selector: 'app-home',
-  styleUrl: './home.component.css',
+  standalone: true,
+  imports: [HeroComponent, BenefitsComponent, ProductCardComponent],
   templateUrl: './home.component.html',
+  styleUrl: './home.component.css',
 })
-export class HomeComponent {}
+export class HomeComponent {
+  private readonly productService = inject(ProductService);
+  private readonly cartService = inject(CartService);
+
+  readonly products = this.productService.products;
+
+  onAddToCart(product: Product): void {
+    this.cartService.addToCart(product);
+  }
+}
