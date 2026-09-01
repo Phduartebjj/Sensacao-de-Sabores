91d060428106ba198325a5541dcf857f6b6edcc6
maoel <pedrohduarte007@gmail.com>
2026-08-31 21:07:23 -0300

    feat(shared): implement product card component and price formatting pipe

---
+++ src/app/shared/pipes/preco-formatado-pipe/preco-formatado-pipe.ts
@@ -0,0 +1,13 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'precoFormatadoPipe',
+})
+export class PrecoFormatadoPipe implements PipeTransform {
+  transform(value: number | undefined): string {
+    if (value === undefined) {
+      return '';
+    }
+    return `R$ ${value.toFixed(2).replace('.', ',')}`;
+  }
+}
