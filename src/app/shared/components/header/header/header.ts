6513ca032c2840098f0c25ea1ed8234706f39b70
maoel <pedrohduarte007@gmail.com>
2026-09-01 21:02:06 -0300

    feat(header): implement dinamyc cart items count

--- src/app/shared/components/header/header/header.ts
+++ src/app/shared/components/header/header/header.ts
@@ -1,9 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { CartService } from '../../../../core/services/cart/cart.service';
+import { RouterLink } from '@angular/router';
 
 @Component({
-  imports: [],
+  imports: [RouterLink],
   selector: 'app-header',
   styleUrl: './header.css',
   templateUrl: './header.html',
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  private readonly cartService = inject(CartService);
+
+  cartItemCount = this.cartService.totalItems;
+}
