b5ff6c1b8eb2217f731c58ecd84efbc0ee563116
Duarte <pedrohduarte007@gmail.com>
2026-09-02 09:24:00 -0300

    feat(badge): Add link input

--- src/app/shared/components/badge/badge.ts
+++ src/app/shared/components/badge/badge.ts
@@ -11,4 +11,5 @@ export class BadgeComponent {
   icon = input<string>('assets/icons/heart-seal.svg');
   title = input<string>('Feito com');
   subtitle = input<string>('Carinho');
+  link = input<string>('#');
 }
