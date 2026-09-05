a747c70afbf6e2eb81ac76ddee942d5778c0801b
maoel <pedrohduarte007@gmail.com>
2026-09-04 22:43:27 -0300

    feat(notification): implement NotificationService and its tests feat(toast): create Toast component with HTML, CSS, and tests

---
+++ src/app/shared/components/toast/toast.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-toast',
+  styleUrl: './toast.css',
+  templateUrl: './toast.html',
+})
+export class Toast {}
